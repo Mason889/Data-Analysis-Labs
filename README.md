f118b3e62fa341cdc00c96e618c16c2b78d0dc0f
Mason889 <kostya.zaigraev@gmail.com>
2021-02-16 00:00:57 +0200

    Lab02 - xml to json converting

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Requirements:
 * Linux host
 * Internet connection
 ## Lab02
+Requirements:
+* Python 3.8.5
+
+Also use `pip install pandas-read-xml` before running `xml_convert.py` Python script.
---
+++ lab02/xml_convert.py
@@ -0,0 +1,8 @@
+import pandas_read_xml as pdx
+
+path = "/Users/install/Data-Analysis/"                                  # your path
+
+df = pdx.read_xml(f'{path}/lab01/final.xml')
+
+with open(f'{path}/lab02/final.json', 'w', encoding='utf-8') as g:
+    g.write(df.to_json())
