8d77619ead1544d3e5ed1cc4352d01d80c2dae3e
Mason889 <kostya.zaigraev@gmail.com>
2021-02-22 01:31:56 +0200

    refactoring

--- lab03/es_import.py
+++ lab03/es_import.py
@@ -14,4 +14,5 @@ with open(f'{path}/lab02/final.json') as rss_json:
             "description": f'{element["description"]}',
             "pubDate": f'{element["pubDate"]}' if 'pubDate' in element else f'{datetime.datetime.now().strftime("%a, %d %b %Y %H:%M:%S")}'
         })
-        print(f'RSS feed was successfully uploaded with _id={json.loads(r.text)["_id"]}') if r.status_code == 201 else print(f'Some error with upload: {r.text}')
+        #print(f'RSS feed was successfully uploaded with _id={json.loads(r.text)["_id"]}')  # debug & visualize
+        print(f'Some error with upload: {r.text}') if r.status_code != 201 else ''
